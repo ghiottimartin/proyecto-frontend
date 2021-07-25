bfbaa189a78a255d85f81b5308d4841feb2b7742
martingh15 <martinghiotti@yahoo.com.ar>
2021-07-25 17:47:05 -0300

    Cambio constantes de backend a mano

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,27 +1,12 @@
 /**
  * Created by martingh15 on 21/04/2020.
  */
-
-//localhost
-let BASE_URL = "";
-let BASE_PUBLIC = "";
-if (process.env.NODE_ENV === 'development' && !process.env.REACT_APP_ENVI) {
+let develop = false;
+let BASE_URL = "https://ancient-oasis-22601.herokuapp.com/api";
+let BASE_PUBLIC = "https://ancient-oasis-22601.herokuapp.com/";
+if (develop) {
     BASE_URL = "http://localhost:8000/api";
     BASE_PUBLIC = "http://localhost:8000/";
-}
-
-
-//testing
-if (process.env.REACT_APP_ENVI === 'develop') {
-    BASE_URL = "https://ancient-oasis-22601.herokuapp.com/api";
-    BASE_PUBLIC = "https://ancient-oasis-22601.herokuapp.com/";
-}
-
-
-//Produccion
-if (process.env.NODE_ENV === 'production' && !process.env.REACT_APP_ENVI) {
-    BASE_URL = "https://ancient-oasis-22601.herokuapp.com/api";
-    BASE_PUBLIC = "https://ancient-oasis-22601.herokuapp.com/";
 }
 
 export default {
