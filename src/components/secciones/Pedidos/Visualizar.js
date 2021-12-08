f83c58c028186507457285e43b47574ea26370af
martingh15 <martinghiotti@yahoo.com.ar>
2021-12-08 12:48:44 -0300

    Visualizar pedido responsive

--- src/components/secciones/Pedidos/Visualizar.js
+++ src/components/secciones/Pedidos/Visualizar.js
@@ -1,43 +1,38 @@
-import React from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import React, { useEffect } from "react"
+import { connect } from "react-redux"
+import { withRouter } from "react-router-dom"
 
 //Actions
-import {fetchPedidoById, resetUpdatePedido } from "../../../actions/PedidoActions";
+import { fetchPedidoById, resetUpdatePedido } from "../../../actions/PedidoActions"
 
 //Components
-import Titulo from "../../elementos/Titulo";
-import Loader from "../../elementos/Loader";
+import Titulo from "../../elementos/Titulo"
+import Loader from "../../elementos/Loader"
 
 //Constantes
-import * as roles from '../../../constants/roles.js';
-import * as rutas from "../../../constants/rutas";
+import * as roles from '../../../constants/roles.js'
+import * as rutas from "../../../constants/rutas"
 
 //CSS
-import "../../../assets/css/Pedidos.css";
+import "../../../assets/css/Pedidos.css"
 
-class Visualizar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+function Visualizar(props) {
+
+    useEffect(() => {
+        const pedido = props.pedidos.update.activo
+        if (pedido && !pedido.id) {
+            props.fetchPedidoById(props.match.params.id)
         }
-    }
-    
-    componentDidMount() {
-        if (this.props.pedidos.update.activo && !this.props.pedidos.update.activo.id) {
-            this.props.fetchPedidoById(this.props.match.params.id);
+        return function limpiarAlta() {
+            props.resetUpdatePedido()
         }
-    }
+    }, [props.match.params.id])
 
-    componentWillUnmount() {
-        this.props.resetUpdatePedido();
-    }
-
-    getVisualizarHtml(pedido, mostrarUsuarios) {
+    const getVisualizarHtml = (pedido, mostrarUsuarios) => {
         if (!pedido || !pedido.lineas) {
-            return "";
+            return ""
         }
-        let filas = [];
+        let filas = []
         pedido.lineas.forEach(linea => {
             filas.push(
                 <tr key={linea.id}>
@@ -46,8 +41,30 @@ class Visualizar extends React.Component {
                     <td className="text-right">{linea.producto.precio_texto}</td>
                     <td className="text-right">{linea.total_texto}</td>
                 </tr>
-            );
-        });
+            )
+        })
+
+        let listaResponsive = []
+        pedido.lineas.forEach(linea => {
+            listaResponsive.push(
+                <li key={linea.id + "-responsive"} className="list-group-item">
+                    <span>{linea.cantidad} x </span>
+                    <span>{linea.producto.precio_texto}</span>
+                </li>
+            )
+            listaResponsive.push(
+                <li key={linea.id + "-responsive-producto"} className="d-flex justify-content-between list-group-item border-black">
+                    <span>{linea.producto.nombre}</span>
+                    <span className="text-right">{linea.total_texto}</span>
+                </li>
+            )
+        })
+        listaResponsive.push(
+            <li className="list-group-item">
+                <label>Total:</label>
+                <span>{pedido.total_texto}</span>
+            </li>
+        )
         return (
             <div className="pedido-visualizar mt-4">
                 <ul>
@@ -55,14 +72,18 @@ class Visualizar extends React.Component {
                         <label>Fecha:</label>
                         <span>{pedido.fecha_texto}</span>
                     </li>
-                    <li style={{display: mostrarUsuarios ? "block" : "none"}}>
+                    <li>
+                        <label>Tipo:</label>
+                        <span>{pedido.tipo_texto}</span>
+                    </li>
+                    <li style={{ display: mostrarUsuarios ? "block" : "none" }}>
                         <label>Usuario:</label>{pedido.usuario_nombre} <span className="texto-chico">({pedido.usuario_email})</span>
                     </li>
                     <li>
                         <label>Estado:</label>
                         <span className={pedido.estado_clase}>{pedido.estado_texto}</span>
                     </li>
-                    <li>
+                    <li className="item-total">
                         <label>Total:</label>
                         <span>{pedido.total_texto}</span>
                     </li>
@@ -86,8 +107,13 @@ class Visualizar extends React.Component {
                         </tr>
                     </tfoot>
                 </table>
+                <div className="pedido-responsive">
+                    <ul className="list-group">
+                        {listaResponsive}
+                    </ul>
+                </div>
             </div>
-        );
+        )
     }
 
     /**
@@ -96,35 +122,33 @@ class Visualizar extends React.Component {
      * 
      * @returns {Boolean}
      */
-    comprobarMostrarUsuario() {
-        let rol = this.props.match.params.rol;
-        let rolVendedor = rol === roles.ROL_VENDEDOR;
-        return rolVendedor;
+    const comprobarMostrarUsuario = () => {
+        let rol = props.match.params.rol
+        let rolVendedor = rol === roles.ROL_VENDEDOR
+        return rolVendedor
     }
 
-    render() {
-        const buscando = this.props.pedidos.byId.isFetchingPedido;
-        let pedido = this.props.pedidos.update.activo;
-        let titulo = "Visualizar Pedido";
-        if (pedido && pedido.id) {
-            titulo += " P" + pedido.id.toString().padStart(5, 0);;
-        }
-        let mostrarUsuario = this.comprobarMostrarUsuario();
-        let html = this.getVisualizarHtml(pedido, mostrarUsuario);
-        let rutaVolver = mostrarUsuario ? rutas.PEDIDOS_VENDEDOR : rutas.PEDIDOS_COMENSAL;
-        return (
-            <div className="tarjeta-body pedido-visualizar">
-                <Titulo ruta={rutaVolver} titulo={titulo} clase="tabla-listado-titulo" />
-                {buscando ? <Loader display={true} /> : html}
-            </div>
-        )
+    const buscando = props.pedidos.byId.isFetchingPedido
+    let pedido = props.pedidos.update.activo
+    let titulo = "Visualizar Pedido"
+    if (pedido && pedido.id) {
+        titulo += " P" + pedido.id.toString().padStart(5, 0)
     }
+    let mostrarUsuario = comprobarMostrarUsuario()
+    let html = getVisualizarHtml(pedido, mostrarUsuario)
+    let rutaVolver = mostrarUsuario ? rutas.PEDIDOS_VENDEDOR : rutas.PEDIDOS_COMENSAL
+    return (
+        <div className="tarjeta-body pedido-visualizar">
+            <Titulo ruta={rutaVolver} titulo={titulo} clase="tabla-listado-titulo" />
+            {buscando ? <Loader display={true} /> : html}
+        </div>
+    )
 }
 
 function mapStateToProps(state) {
     return {
         pedidos: state.pedidos,
-    };
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
@@ -136,5 +160,5 @@ const mapDispatchToProps = (dispatch) => {
             dispatch(resetUpdatePedido())
         }
     }
-};
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Visualizar));
+}
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Visualizar))
